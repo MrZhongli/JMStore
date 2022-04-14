1e22bc1a6c818353d7a49dbfa15d5e3a4e98edd0
kenneth chan <david.camachokd@gmail.com>
2022-04-14 03:04:40 -0300

    Structure NavBar and footer with tailwind and daisyui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
+  const styles= {
+    header:"text-3xl font-bold underline text-orange-500"
+  }
+
   return (
     <div className="App">
       <NavBar/>
-      <h1>hola mundo</h1>
+      <h1 className="">This is JMStore on dev</h1>
+      <h1 className={styles.header}>
+      This is JMStore on dev and this is a proof
+    </h1>
+  
+    <Footer/>
     </div>
   );
 }
