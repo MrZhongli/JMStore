b2b3da35dc77d29ec39ff17957a104bbc250c9d9
kenneth chan <david.camachokd@gmail.com>
2022-04-13 21:52:46 -0300

    Initializing new react project

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <div>
+        <li>Menu</li>
+        <li>Producto</li>
+        <li>Contactos</li>
+        <li>Servicios</li>
+    </div>
+  )
+}
+
+export default NavBar
