826d599dd854d0ea43377f4fef38c245a2e6dd83
kenneth chan <david.camachokd@gmail.com>
2022-05-28 04:09:15 -0300

    env variable

--- src/components/firebase/firebaseClient.jsx
+++ src/components/firebase/firebaseClient.jsx
@@ -3,13 +3,16 @@ import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID
+
+
+  apiKey: "AIzaSyBf52AIL_Cf9OYFduTexrjiVtb7ncfI1DE",
+  authDomain: "rosse-papeleria.firebaseapp.com",
+  projectId: "rosse-papeleria",
+  storageBucket: "rosse-papeleria.appspot.com",
+  messagingSenderId: "988501034416",
+  appId: "1:988501034416:web:f549125b7d8975caae0275",
+  measurementId: "G-LM6NLK6XTC"
+
 };
 
 // Initialize Firebase
